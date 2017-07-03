37ec5ddcc21fa059de6eaedf3302471f625a4c3f
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-02 20:40:44 -0400

    create the edit, update and linking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,12 +17,31 @@ class ArticlesController < ApplicationController
 		end  	
 		# @article.save
 		# redirect_to articles_path(@article)
-  end	
+  end
+
 
   def show
     @article = Article.find(params[:id])
+  end	
 
+	def edit
+    @article = Article.find(params[:id])
 	end	
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(@article)
+		else
+		  render 'new'	
+		end  
+  end
+
+  def index
+    @articles = Article.all
+
+  end	
 
 	private
 	  def article_params
