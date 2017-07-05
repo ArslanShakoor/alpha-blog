4cb009466011a1cd7ecd0d5999b4b7f852ff8366
Arslan Shakoor <arslantechielive@gmail.com>
2017-07-05 14:36:40 -0400

    added profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,34 @@ class UsersController  < ApplicationController
     @user = User.new(user_params)
 
 		if @user.save
-			flash[:success] = "User was successfully created"
+			flash[:success] = "Congrats #{@user.username}, Welcome to Alpha Blog"
 			redirect_to articles_path
 		else
 			 render 'new'
 		end  	
 	end
+
+	def show
+    @user = User.find(params[:id])
+    
+
+	end	
+
+	def edit
+		@user = User.find(params[:id])
+  end
+
+	def update
+		@user = User.find(params[:id])
+ 
+		if @user.update(user_params)
+			flash[:success] = "User was successfully updated"
+			redirect_to articles_path
+		else
+		  render 'edit'	
+	end  
+
+	end	
 
 	private
 	  def set_article
